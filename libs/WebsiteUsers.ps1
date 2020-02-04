8f3435652e9167c0e95cf63fb3efc8c402cade80
vabalcar <vabalcar@gmail.com>
2020-02-04 16:17:48 +0100

    Added comments to WebsiteUsers lib

--- libs/WebsiteUsers.ps1
+++ libs/WebsiteUsers.ps1
@@ -1,15 +1,34 @@
+<#
+.SYNOPSIS
+The library for batch managing website users in Virtualmin
+
+.PARAMETER ConfigurationFile
+A configuration file to load
+#>
+
 Param (
     [Parameter(Mandatory=$true)] [string] $ConfigurationFile
 )
 
+# Load configuration.
 $config = Get-Content -Path $ConfigurationFile | ConvertFrom-Json
 
+<#
+.SYNOPSIS
+Tests if a website user exists in a domain from a configuration.
+
+.PARAMETER Negate
+Negates the output.
+
+.INPUTS
+Name of the user to be tested.
+#>
 filter Test-WebsiteUser {
     Param(
         [switch] $Negate,
         [Parameter(ValueFromPipeline=$true)] [string] $Name
     )
-	
+    
     if ((([string](& virtualmin list-users --domain $config.domain --name-only --user $Name)).Length -gt 0) -xor $Negate) {
         $Name
     } elseif ($Negate) {
@@ -19,22 +38,45 @@ filter Test-WebsiteUser {
     }
 }
 
+<#
+.SYNOPSIS
+Tests if a supplied DB exists.
+
+.PARAMETER Negate
+Negates the output.
+
+.INPUTS
+Name of DB to be tested
+#>
 filter Test-DB {
-	Param(
-		[switch] $Negate,
-		[Parameter(ValueFromPipeline=$true)] [string] $Name
-	)
-	
-	$db = & mysqlshow | ForEach-Object { $_.Split(' ')[1] } | Select-Object -Skip 2 | Select-String -Pattern "^$Name$"
-	if (($db -eq $Name) -xor $Negate) {
-		$Name
-	} elseif ($Negate) {
-		"DB $Name already exists!" | Out-Host
-	} else {
-		"DB $Name does not exists!" | Out-Host
-	}
+    Param(
+        [switch] $Negate,
+        [Parameter(ValueFromPipeline=$true)] [string] $Name
+    )
+    
+    $db = & mysqlshow | ForEach-Object { $_.Split(' ')[1] } | Select-Object -Skip 2 | Select-String -Pattern "^$Name$"
+    if (($db -eq $Name) -xor $Negate) {
+        $Name
+    } elseif ($Negate) {
+        "DB $Name already exists!" | Out-Host
+    } else {
+        "DB $Name does not exists!" | Out-Host
+    }
 }
 
+<#
+.SYNOPSIS
+Creates an MYSQL-safe name from a potentially unsafe one.
+
+.PARAMETER Name
+Potentially unsafe name of a DB
+
+.INPUTS
+You cannot pipe objects to Get-DBName.
+
+.OUTPUTS
+Name of a DB based on given one
+#>
 function Get-DBName {
     Param (
         [Parameter(Mandatory=$true)] [string] $Name
@@ -45,7 +87,20 @@ function Get-DBName {
     }
 }
 
-function Get-GroupName {
+<#
+.SYNOPSIS
+Retrieves a name of a group in which a user with a supplied name belongs.
+
+.PARAMETER Name
+The name of the user
+
+.INPUTS
+You cannot pipe objects to Get-Group.
+
+.OUTPUTS
+The name of the group in which the user with the supplied name belongs
+#>
+function Get-Group {
     Param (
         [Parameter(Mandatory=$true)] [string] $Name
     )
@@ -57,6 +112,25 @@ function Get-GroupName {
     }
 }
 
+<#
+.SYNOPSIS
+Creates Apache configuration for a user.
+
+.PARAMETER Name
+The name of the user
+
+.PARAMETER HomeDir
+The home dir of the user
+
+.PARAMETER LocationBase
+The path in the URL of the user's website
+
+.INPUTS
+You cannot pipe objects to Get-ApacheUserConfig.
+
+.OUTPUTS
+Apache configuration for a user
+#>
 function Get-ApacheUserConfig {
     Param(
         [Parameter(Mandatory=$true)] [string] $Name,
@@ -75,9 +149,28 @@ Redirect "/$Name" "https://$($config.domain)/$LocationBase/$Name"
 
 }
 
+<#
+.SYNOPSIS
+Creates a website user on the server.
+
+.PARAMETER Group
+The group in which the user has to be created
+
+.PARAMETER Name
+The name of the user
+
+.PARAMETER DryRun
+If set, nothing will be created and only output messages will be shown.
+
+.INPUTS
+You cannot pipe objects to New-WebsiteUser.
+
+.OUTPUTS
+Nothing
+#>
 function New-WebsiteUser {
     Param (
-        [Parameter(Mandatory=$true)] [string] $GroupName,
+        [Parameter(Mandatory=$true)] [string] $Group,
         [Parameter(ValueFromPipeline=$true)] [string] $Name,
         [switch] $DryRun
     )
@@ -85,8 +178,10 @@ function New-WebsiteUser {
     begin {
         $usersCreated = 0
         if (!$DryRun) {
-            $locationBase = Join-Path $config.websitesLocation $GroupName
+            $locationBase = Join-Path $config.websitesLocation $Group
             $homeDirBase = Join-Path $config.apacheServerDocumentRoot $locationBase
+            
+            # Create a group's dir iff necessary.
             if (!(Test-Path -PathType Container -Path $homeDirBase)) {
                 New-Item -ItemType 'Directory' -Path $homeDirBase | Out-Null
                 & chown -R "$($config.serverUser):$($config.serverGoup)" $homeDirBase
@@ -95,9 +190,10 @@ function New-WebsiteUser {
     }
 
     process {
-        "Creating user $Name in group $GroupName..." | Out-Host
+        "Creating user $Name in group $Group..." | Out-Host
         if ($DryRun) { return }
         
+        # Create a user's home dir structure.
         $homeDir = Join-Path $homeDirBase $Name
         if (!(Test-Path -PathType Container -Path $homeDir)) {
             New-Item -ItemType 'Directory' -Path $homeDir | Out-Null
@@ -106,19 +202,24 @@ function New-WebsiteUser {
             New-Item -Force -ItemType Directory -Path "$homeDir/.tmp" | Out-Null
         }
         
+        # Give ownership of the user's home dir to the server user.
+        & chown -R "$($config.serverUser):$($config.serverGoup)" $homeDir
+        
+        # Create a user's database.
         $dbname = Get-DBName -Name $Name
         & virtualmin create-database --domain $config.domain --name $dbName --type mysql
         
+        # Create the user itself.
         & virtualmin create-user --domain $config.domain --user $Name --pass $config.defaultUserPassword --ftp --noemail --mysql $dbName --web --home (Join-Path 'public_html' $locationBase $Name)
 
+        # Create a user's Apache configuration.
         Get-ApacheUserConfig -Name $Name -HomeDir $homeDir -LocationBase $locationBase | Out-File -Path (Join-Path $config.apacheUsersSettings "$Name.conf")
-        
-        & chown -R "$($config.serverUser):$($config.serverGoup)" $homeDir
         
         ++$usersCreated
     }
     
     end {
+        # Restart Apache iff it's necessary.
         if ($usersCreated -gt 0) {
             & /etc/init.d/apache2 restart
         }
@@ -127,7 +228,7 @@ function New-WebsiteUser {
 
 function Restore-WebsiteUser {
     Param (
-        [Parameter(Mandatory=$true)] [string] $GroupName,
+        [Parameter(Mandatory=$true)] [string] $Group,
         [Parameter(ValueFromPipeline=$true)] [string] $Name,
         [switch] $DryRun
     )
@@ -135,13 +236,13 @@ function Restore-WebsiteUser {
     begin {
         $usersFixed = 0
         if (!$DryRun) {
-            $locationBase = Join-Path $config.websitesLocation $GroupName
+            $locationBase = Join-Path $config.websitesLocation $Group
             $homeDirBase = Join-Path $config.apacheServerDocumentRoot $locationBase
         }
     }
 
     process {
-        "Fixing user $Name in group $GroupName..." | Out-Host
+        "Fixing user $Name in group $Group..." | Out-Host
         if ($DryRun) { return }
         
         $homeDir = Join-Path $homeDirBase $Name
@@ -153,12 +254,29 @@ function Restore-WebsiteUser {
     }
     
     end {
+        # Restart Apache iff it's necessary.
         if ($usersFixed -gt 0) {
             & /etc/init.d/apache2 restart
         }
     }
 }
 
+<#
+.SYNOPSIS
+Removes a website user on the server.
+
+.PARAMETER Name
+The name of the user
+
+.PARAMETER DryRun
+If set, nothing will be removed and only output messages will be shown.
+
+.INPUTS
+You cannot pipe objects to Remove-WebsiteUser.
+
+.OUTPUTS
+Nothing
+#>
 function Remove-WebsiteUser {
     Param (
         [Parameter(ValueFromPipeline=$true)] [string] $Name,
@@ -170,23 +288,28 @@ function Remove-WebsiteUser {
     }
     
     process {
-		$groupName = Get-GroupName -Name $Name
-        "Removing user $_ from group $groupName..." | Out-Host
+        $group = Get-Group -Name $Name
+        "Removing user $_ from group $group..." | Out-Host
         if ($DryRun) { return }
 
+        # Remove the user's Apache configuration.
+        Remove-Item -Force -Path (Join-Path $config.apacheUsersSettings "$Name.conf")
+
+        # Remove user itself.
         & virtualmin delete-user --domain $config.domain --user $Name
 
+        # Remove the user's database.
         $dbname = Get-DBName -Name $Name
         & virtualmin delete-database --domain $config.domain --name $dbName --type mysql
 
-        Remove-Item -Force -Path (Join-Path $config.apacheUsersSettings "$Name.conf")
-
-        $homeDirBase = Join-Path $config.apacheServerDocumentRoot $config.websitesLocation $groupName
+        # Remove the user's home dir.
+        $homeDirBase = Join-Path $config.apacheServerDocumentRoot $config.websitesLocation $group
         $homeDir = Join-Path $homeDirBase $Name
         if (Test-Path -PathType Container -Path $homeDir) {
             Remove-Item -Recurse -Force -Path $homeDir
         }
 
+        # Remove the group's dir iff it's empty.
         if ((!(Test-Path -Path (Join-Path $homeDirBase '*'))) -and (Test-Path -Path $homeDirBase)) {
             Remove-Item -Recurse -Force -Path $homeDirBase
         }
@@ -195,12 +318,29 @@ function Remove-WebsiteUser {
     }
     
     end {
+        # Restart Apache iff it's necessary.
         if ($usersRemoved -gt 0) {
             & /etc/init.d/apache2 restart
         }
     }
 }
 
+<#
+.SYNOPSIS
+Backups a website user on the server.
+
+.PARAMETER Name
+The name of the user
+
+.PARAMETER DryRun
+If set, nothing will be created and only output messages will be shown.
+
+.INPUTS
+You cannot pipe objects to Backup-WebsiteUser.
+
+.OUTPUTS
+Nothing
+#>
 function Backup-WebsiteUser {
     Param (
         [Parameter(ValueFromPipeline=$true)] [string] $Name,
@@ -208,25 +348,29 @@ function Backup-WebsiteUser {
     )
     
     process {
-        $groupName = Get-GroupName -Name $Name
-        "Backing up data of user $Name from group $groupName..." | Out-Host
+        $group = Get-Group -Name $Name
+        "Backing up data of user $Name from group $group..." | Out-Host
         if ($DryRun) { return }
 
-        $locationBase = Join-Path $config.websitesLocation $groupName
+        # Create a backup dir iff necessary.
+        $locationBase = Join-Path $config.websitesLocation $group
         $backupDir = Join-Path $config.backupDir $locationBase
         if (!(Test-Path -PathType Container -Path $backupDir)) {
             New-Item -ItemType 'Directory' -Path $backupDir | Out-Null
         }
 
+        # Backup the user's home dir to zip archive in the backup dir iff necessary.
         $homeDirBase = Join-Path $config.apacheServerDocumentRoot $locationBase
         $homeDir = Join-Path $homeDirBase $Name
         if (Test-Path -PathType Container -Path $homeDir) {
             & zip -r (Join-Path $backupDir "$Name.zip") (Join-Path $homeDir '*')
         }
         
+        # Backup the user's database to a SQL script.
         $dbname = Get-DBName -Name $Name
         & mysqldump --databases $dbName | Out-File -Path (Join-Path $backupDir "$dbName.sql")
         
+        # Give ownership of the backup dir to the server user.
         & chown -R "$($config.serverUser):$($config.serverGoup)" $backupDir
     }
 }
